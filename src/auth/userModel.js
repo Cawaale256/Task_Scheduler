11be20f5d5c629ea8b7a4dc11199e39e5e6642d2
Abdi Awale <abdi.awale5@gmail.com>
2025-01-19 22:01:51 +0000

    Add bcrypt password hashing to user schema pre-save middleware

--- src/auth/userModel.js
+++ src/auth/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.userSchema ({
     username: {
@@ -15,6 +16,16 @@ const userSchema = new mongoose.userSchema ({
         enum: ['user', 'admin'],
         default: user
     }
+});
+
+// Hash the password before saving the user model
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 const user=mongoose.model('user', 'userSchema');
