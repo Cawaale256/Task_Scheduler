7609e55e18820cad74cf1372150b2032c3439a4c
Abdi Awale <abdi.awale5@gmail.com>
2025-03-01 17:18:35 +0000

    Update Task: A function that updates an existing task by its ID with the new values.

--- src/tasks/ taskService.js
+++ src/tasks/ taskService.js
@@ -4,13 +4,37 @@ const Task = require('./taskModel')
 
 // Create a new task
 const createTask = async(taskData) => {
-    const {title, descrption, dueDate, priority}= taskData;
+    const {title, description, dueDate, priority}= taskData;
     const newTask = new Task([
         title,
-        RTCSessionDescription,
+        description,
         dueDate,
         priority
     ])
     return await newTask.save();
 };
 
+// Read tasks based on criteria or get all tasks if no criteria
+
+const readTask = async(criteria = {}) => {
+    const {title, description, dueDate, priority} = criteria;
+    let searchFilter = {};
+    if(title) searchFilter.title = title;
+    if(description) searchFilter.description = description;
+    if(dueDate) searchFilter.dueDate = new Date(dueDate);
+    if(priority) searchFilter.priority = priority;
+
+    return await Task.find(searchFilter);
+}
+
+// Update an existing task by ID
+const updateTask = async(IdleDeadline, updates)=>{
+    const task = await Task.findById(id);
+    if (!task) {
+        throw new Error('Task not found');
+    }
+    Object.keys(updates).forEach((key) => {
+        task[key] = updates[key];
+    });
+    return await task.save();
+}
